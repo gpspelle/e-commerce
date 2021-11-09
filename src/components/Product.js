86affd37e61499bbcbd739bd945492d717afcc63
gpspelle <gpsunicamp016@gmail.com>
2021-11-09 17:23:20 -0300

    apply eslint on a missing file

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,32 +1,31 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Badge from "react-bootstrap/Badge";
+import React from "react"
+import { useHistory } from "react-router-dom"
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import Badge from "react-bootstrap/Badge"
 
-export const PRODUCT_DESCRIPTION = "product-description";
+export const PRODUCT_DESCRIPTION = "product-description"
 
 export default function Product({ identifier, description, price }) {
-     const history = useHistory();
+  const history = useHistory()
 
-    const openDetailPage = (event) => {
-        console.log(event);
-        history.push(`${PRODUCT_DESCRIPTION}/${identifier}`);
-    }
+  const openDetailPage = (event) => {
+    history.push(`${PRODUCT_DESCRIPTION}/${identifier}`)
+  }
 
-    return (
-    <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="artesanato.jpg" />
-        <Card.Body>
-            <Card.Title>{identifier}</Card.Title>
-            <Card.Text>
-                {description}
-            </Card.Text>
-            <Button variant="primary" onClick={(event) => openDetailPage(event)}>Mais detalhes</Button>
-            <Badge pill bg="success">
-                {price} R$
-            </Badge>
-        </Card.Body>
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="artesanato.jpg" />
+      <Card.Body>
+        <Card.Title>{identifier}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary" onClick={(event) => openDetailPage(event)}>
+          Mais detalhes
+        </Button>
+        <Badge pill bg="success">
+          {price} R$
+        </Badge>
+      </Card.Body>
     </Card>
-    );
+  )
 }
