92a70e534ee9fa639aac317080abf4f3e4f38d32
gpspelle <gpsunicamp016@gmail.com>
2021-11-09 18:34:54 -0300

    add api gateway deployment to retrieve products from dynamodb

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,23 +1,58 @@
-import * as dynamodb from '@aws-cdk/aws-dynamodb';
-import * as iam from '@aws-cdk/aws-iam';
-import * as cdk from '@aws-cdk/core';
+import * as dynamodb from "@aws-cdk/aws-dynamodb"
+import * as apigateway from "@aws-cdk/aws-apigateway"
+import * as lambda from "@aws-cdk/aws-lambda"
+import * as path from "path"
+import * as cdk from "@aws-cdk/core"
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // 👇 create Dynamodb table
     const table = new dynamodb.Table(this, id, {
-      tableName: 'products',
+      tableName: "products",
       billingMode: dynamodb.BillingMode.PROVISIONED,
       readCapacity: 1,
       writeCapacity: 1,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
       pointInTimeRecovery: true,
-    });
+    })
 
-    console.log('table name 👉', table.tableName);
-    console.log('table arn 👉', table.tableArn);
+    console.log("table name 👉", table.tableName)
+    console.log("table arn 👉", table.tableArn)
+
+    const api = new apigateway.RestApi(this, "api", {
+      description: "e-commerce api gateway",
+      deployOptions: {
+        stageName: "dev",
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key"],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      },
+    })
+
+    // 👇 create an Output for the API URL
+    new cdk.CfnOutput(this, "apiUrl", { value: api.url })
+
+    // 👇 define GET products function
+    const getProductsLambda = new lambda.Function(this, "get-products-lambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.main",
+      code: lambda.Code.fromAsset(path.join(__dirname, "/../src/get-products")),
+    })
+
+    // 👇 add a /products resource
+    const products = api.root.addResource("products")
+
+    // 👇 integrate GET /products with getProductsLambda
+    products.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(getProductsLambda, { proxy: true })
+    )
   }
 }
