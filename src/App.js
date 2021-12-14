660821986f0365cf1a7ba7d970f7e90b18f3deec
gpspelle <gpsunicamp016@gmail.com>
2021-12-13 23:46:01 -0300

    change favicon, title, description

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+import DocumentMeta from "react-document-meta"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { PRODUCT_DESCRIPTION } from "./components/Product/Product"
 import ProductContainer from "./components/ProductContainer/ProductContainer"
 import ProductDescription from "./components/ProductDescription/ProductDescription"
+import { PAGE_DESCRIPTION, PAGE_TITLE } from "./constants/constants"
+
+const meta = {
+  title: PAGE_TITLE,
+  description: PAGE_DESCRIPTION,
+  meta: {
+    charset: "utf-8",
+  },
+}
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path={`/:id/${PRODUCT_DESCRIPTION}`}>
-          <ProductDescription />
-        </Route>
-        <Route path="/">
-          <ProductContainer />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Switch>
+          <Route path={`/:id/${PRODUCT_DESCRIPTION}`}>
+            <ProductDescription />
+          </Route>
+          <Route path="/">
+            <ProductContainer />
+          </Route>
+        </Switch>
+      </Router>
+      <DocumentMeta {...meta} />
+    </div>
   )
 }
 
