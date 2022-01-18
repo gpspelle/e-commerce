3f75bc9f539ba22059f2ee99d1b7606d3753f20f
gpspelle <gpsunicamp016@gmail.com>
2022-01-17 23:37:18 -0300

    reduce loading time css

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import "./react-bootstrap.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-      crossOrigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-    />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
